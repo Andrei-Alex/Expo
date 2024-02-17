4ee9f1a8775cdbd43f651be03d2e4372b403ca2b
Andrei-Alex <Andrei-Alex@outlook.com>
2024-02-17 09:52:33 +0200

    refactoring

--- app/screens/ViewImageScreen/ViewImageScreen.tsx
+++ app/screens/ViewImageScreen/ViewImageScreen.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Image, ImageBackground, Text, View } from "react-native";
+
 import { styles } from ".";
 
 function ViewImageScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.closeIcon}></View>
+        <View style={styles.deleteIcon}></View>
       <Image
         style={styles.image}
         resizeMode={"contain"}
--- app/screens/ViewImageScreen/styles.ts
+++ app/screens/ViewImageScreen/styles.ts
@@ -1,17 +1,25 @@
 import { StyleSheet } from "react-native";
-
+import {colors} from '../../config'
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: "black",
+    backgroundColor:  colors.black,
     flex: 1,
   },
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#fc5c65",
+    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 40,
+    right: 30,
   },
   image: {
     width: "100%",
