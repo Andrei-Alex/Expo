b85cb38353e09fff9e3711a40f87b130f1a3bc58
Andrei-Alex <Andrei-Alex@outlook.com>
2024-02-18 09:59:17 +0200

    lint fix

--- app/ui/atoms/AppText/AppText.test.tsx
+++ app/ui/atoms/AppText/AppText.test.tsx
@@ -4,7 +4,6 @@ import renderer from 'react-test-renderer';
 
 import AppText from './AppText';
 
-
 describe('AppText', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<AppText>Test</AppText>).toJSON();
@@ -12,22 +11,26 @@ describe('AppText', () => {
   });
   it('applies style correctly', () => {
     const testStyle = { fontSize: 20, color: 'blue' };
-    const tree = renderer.create(<AppText style={testStyle}>Styled Test</AppText>).toJSON();
+    const tree = renderer
+      .create(<AppText style={testStyle}>Styled Test</AppText>)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
   it('applies passed TextStyle', () => {
     const testStyle = { color: 'red', fontSize: 20 };
 
-    const instance = renderer.create(<AppText style={testStyle}>Test</AppText>).root;
+    const instance = renderer.create(
+      <AppText style={testStyle}>Test</AppText>
+    ).root;
     const textComponent = instance.findByType(Text);
 
     const receivedStyle = textComponent.props.style;
-    const styleArray = Array.isArray(receivedStyle) ? receivedStyle : [receivedStyle];
+    const styleArray = Array.isArray(receivedStyle)
+      ? receivedStyle
+      : [receivedStyle];
 
-    const hasTestStyle = styleArray.some(style =>
-        Object.entries(testStyle).every(([key, value]) =>
-            style[key] === value
-        )
+    const hasTestStyle = styleArray.some((style) =>
+      Object.entries(testStyle).every(([key, value]) => style[key] === value)
     );
     expect(hasTestStyle).toBeTruthy();
   });
