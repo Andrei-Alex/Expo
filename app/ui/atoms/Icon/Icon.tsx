297a346f939fb10c60af87a37da1f373ee27b6ef
Andrei <Andrei-Alex@outlook.com>
2024-02-21 22:18:41 +0200

    Docs

--- app/ui/atoms/Icon/Icon.tsx
+++ app/ui/atoms/Icon/Icon.tsx
@@ -6,6 +6,32 @@ import { colors } from '../../../config';
 
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
+/**
+ * Icon Component
+ *
+ * A reusable and customizable icon component that integrates MaterialCommunityIcons from '@expo/vector-icons' library, providing
+ * a wide range of icons suitable for various application needs. It leverages the flexibility of React Native's View component
+ * to create a container for the icon, which can be customized in size, background color, and icon color. This component
+ * simplifies the process of adding icons to your app, ensuring consistency in design and ease of use.
+ *
+ * ### Usage
+ * ```jsx
+ * <Icon
+ *   name="email"
+ *   size={50}
+ *   backgroundColor="#f0f"
+ *   iconColor="#fff"
+ * />
+ * ```
+ *
+ * @component
+ * @param {Object} props - The properties passed to the component.
+ * @param {string} props.name - The name of the icon to be displayed.
+ * @param {number} [props.size=40] - The size of the icon and its container. Defaults to 40.
+ * @param {string} [props.backgroundColor=colors.black] - The background color of the icon container. Defaults to black.
+ * @param {string} [props.iconColor=colors.white] - The color of the icon. Defaults to white.
+ * @returns {React.ReactElement} A customizable icon component that can be easily integrated into React Native applications.
+ */
 const Icon: React.FC<Props> = ({
   name,
   size = 40,
