b20cb573a8aa5dedf20afc8f8783dfc6d8009a0e
Andrei-Alex <Andrei-Alex@outlook.com>
2024-02-25 13:41:00 +0200

    Docs updated

--- app/ui/features/AppPicker/AppPicker.tsx
+++ app/ui/features/AppPicker/AppPicker.tsx
@@ -12,6 +12,35 @@ import { IAppPicker, styles } from './index';
 import { MainScreen } from '../../layouts';
 import { PickerItem } from '../../components';
 
+/**
+ * AppPicker Component
+ *
+ * A custom picker component for React Native that displays a modal list of selectable items. The component integrates
+ * an optional leading icon, and displays the selected item's label or a placeholder when no item is selected. Tapping
+ * on the component opens a modal with a list of items, each represented by a `PickerItem`. The user can select an item
+ * from the list, which then updates the displayed label and invokes the `onSelectItem` callback with the selected item.
+ *
+ * ### Usage
+ *
+ * ```jsx
+ * <AppPicker
+ *   items={[{ label: 'Item 1', value: 1 }, { label: 'Item 2', value: 2 }]}
+ *   selectedItem={{ label: 'Item 1', value: 1 }}
+ *   onSelectItem={(item) => console.log(item)}
+ *   icon="apps"
+ *   placeholder="Select an item"
+ * />
+ * ```
+ *
+ * @component
+ * @param {IAppPicker} props - The properties passed to the component.
+ * @param {(item: item) => void} props.onSelectItem - Callback function that is invoked with the selected item when an item is selected.
+ * @param {item} props.selectedItem - The currently selected item, used to display the label in the picker field. If no item is selected, the `placeholder` is displayed instead.
+ * @param {item[]} props.items - An array of items that the user can select from. Each item must have a `label` for display and a `value` as a unique identifier.
+ * @param {icons} [props.icon] - Optional. The name of the icon to display on the left side of the picker field, from the predefined set of icons.
+ * @param {string} [props.placeholder] - Optional. The text to display when no item is selected.
+ * @returns {React.ReactElement} A customizable picker component with an optional icon and a modal selection list.
+ */
 const AppPicker: React.FC<IAppPicker> = ({
   onSelectItem,
   selectedItem,
