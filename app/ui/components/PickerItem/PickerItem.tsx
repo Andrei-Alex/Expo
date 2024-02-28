c9671f1e1c00924f88cab20f73bc3a26b6cc30b4
Andrei <Andrei-Alex@outlook.com>
2024-02-28 21:05:45 +0200

    Test updated

--- app/ui/components/PickerItem/PickerItem.tsx
+++ app/ui/components/PickerItem/PickerItem.tsx
@@ -26,7 +26,7 @@ import { IPickerItem, styles } from '.';
  */
 const PickerItem: React.FC<IPickerItem> = ({ label, onPress }) => {
   return (
-    <TouchableOpacity onPress={() => onPress()}>
+    <TouchableOpacity onPress={() => onPress()} testID={label}>
       <AppText style={styles.text}>{label}</AppText>
     </TouchableOpacity>
   );
