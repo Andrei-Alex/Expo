4b2841fadeaf526fd92109adde4fed8265cc7226
Andrei-Alex <Andrei-Alex@outlook.com>
2024-03-04 12:03:26 +0200

    AppFormImagePicker

---
+++ app/ui/modules/AppFormImagePicker/AppFormImagePicker.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+import { useFormikContext } from 'formik';
+
+import { ImageInputList } from '../../features';
+import { ErrorMessage } from '../../components';
+import { IAppFormImagePicker } from '.';
+
+const AppFormImagePicker: React.FC<IAppFormImagePicker> = ({ name }) => {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  const imageUris = values[name];
+  const handleAdd = (uri) => {
+    setFieldValue(name, [...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setFieldValue(
+      name,
+      imageUris.filter((imageURI) => imageURI !== uri)
+    );
+  };
+  return (
+    <>
+      <ImageInputList
+        imageURIs={imageUris}
+        onRemoveImage={handleAdd}
+        onAddImage={handleRemove}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+};
+
+export default AppFormImagePicker;
