ffa204eb0a6d2863e04e62fd656eb4543ae28f33
Andrei-Alex <Andrei-Alex@outlook.com>
2024-03-04 11:31:42 +0200

    ImageInputList

--- app/ui/features/ImageInputList/ImageInputList.tsx
+++ app/ui/features/ImageInputList/ImageInputList.tsx
@@ -1,28 +1,37 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 
 import { styles, IImageInputList } from '.';
 import { ImageInput } from '../../components';
+import { useRef } from 'react';
 
 const ImageInputList: React.FC<IImageInputList> = ({
   imageURIs = [],
   onRemoveImage,
   onAddImage
 }) => {
+  const scrollView = useRef<ScrollView>(null);
+
   return (
-    <>
-      <View style={styles.container}>
-        {imageURIs.map((uri) => (
-          <View style={styles.image} key={uri}>
-            <ImageInput
-              imageUri={uri}
-              onChangeImage={() => onRemoveImage(uri)}
-            />
-          </View>
-        ))}
-        <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
-      </View>
-    </>
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageURIs.map((uri) => (
+            <View style={styles.image} key={uri}>
+              <ImageInput
+                imageUri={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
