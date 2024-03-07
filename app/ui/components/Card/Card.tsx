35eafab3937bdc170b317c8fd72eca9ba4ebe09c
Andrei-Alex <Andrei-Alex@outlook.com>
2024-03-07 13:54:24 +0200

    Fetchfix

--- app/ui/components/Card/Card.tsx
+++ app/ui/components/Card/Card.tsx
@@ -28,11 +28,10 @@ import { AppText } from '../../atoms';
  */
 
 const Card: React.FC<Props> = ({ title, subTitle, image, onPress }) => {
-  console.log(image);
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image source={{ uri: image }} style={styles.image} />
+        <Image source={{ uri: 'http:/' + image }} style={styles.image} />
         <View style={styles.detailsContainer}>
           <AppText style={styles.title} numberOfLines={1}>
             {title}
