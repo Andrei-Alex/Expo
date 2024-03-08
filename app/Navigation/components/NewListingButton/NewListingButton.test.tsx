9bed993f4d3caa994421e8c1ade63361c04fdf26
Andrei <Andrei-Alex@outlook.com>
2024-03-08 19:11:05 +0200

    test added

--- app/Navigation/components/NewListingButton/NewListingButton.test.tsx
+++ app/Navigation/components/NewListingButton/NewListingButton.test.tsx
@@ -2,9 +2,19 @@ import * as React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 
 import NewListingButton from './NewListingButton';
+import * as renderer from 'react-test-renderer';
 
 describe('NewListingButton', () => {
   const onPressMock = jest.fn();
+
+  it('render correctly', () => {
+    const component = renderer.create(
+      <NewListingButton onPress={onPressMock} />
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
   it('call on press function', () => {
     const { getByTestId } = render(<NewListingButton onPress={onPressMock} />);
